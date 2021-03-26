fcac38b9204045a89d6a84b8a00a25fa73a14f69
lwod <lwodcurrentmail@gmail.com>
2021-03-26 21:24:53 +0200

    add useAuth to the App at react

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import 'materialize-css'
 import {useRoutes} from "./routes";
 import {BrowserRouter} from "react-router-dom";
+import {useAuth} from "./hooks/auth.hook";
 
 function App() {
-    
+    const {login, logout, token, userId} = useAuth()
     const routes = useRoutes(false)
     
   return (
