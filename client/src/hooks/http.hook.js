2d1f0354556ee999e82144d19dbd1da54e9bab96
lwod <lwodcurrentmail@gmail.com>
2021-03-26 19:36:14 +0200

    correct flow data from react to server

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -5,7 +5,7 @@ export const useHttp = () =>{
 	const [error, setError] = useState(null)
 	
 	const request = useCallback( async (url, method = 'GET', body = null, headers = {}) => {
-		
+		//
 		setLoading(true);
 		
 		try{
