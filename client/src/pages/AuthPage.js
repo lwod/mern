1ae7783f3615e536898718b11a8a8a002da416f9
lwod <lwodcurrentmail@gmail.com>
2021-03-26 12:45:15 +0200

    basic card at AuthPage.js

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -2,8 +2,20 @@ import React from "react";
 
 export const AuthPage = () =>{
 	return(
-		<div>
-			<h1>Auth Page</h1>
+		<div className={'row'}>
+			<div className={"col s6 offset-s3"}>
+				<h1>Сокращение ссылки</h1>
+				<div className="card blue darken-1">
+					<div className="card-content white-text">
+						<span className="card-title">Авторизация</span>
+						
+					</div>
+					<div className="card-action">
+						<button className={'btn yellow darken-4' } style={{marginRight:10}}>Войти</button>
+						<button className={'btn grey lighten-1 black-text'}>Регистрация</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
