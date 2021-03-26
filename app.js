04ed6bd28e9f9bb20628efe57bce3f3b083a70cf
lwod <lwodcurrentmail@gmail.com>
2021-03-26 19:25:09 +0200

    add json-request parsing to the express

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || config.get('port')
 
 const mongoose = require('mongoose');
 
+app.use(express.json({extended:true}))
+
 app.use('/api/auth', require('./routes/auth.routes'));
 
 app.get("/", async (req,res)=>{
