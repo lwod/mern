03b08da63829e42c326010c62e76eb4c40830979
lwod <lwodcurrentmail@gmail.com>
2021-03-26 21:21:48 +0200

    check if user already login by localStorage at auth.hook.js

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -1,4 +1,4 @@
-import {useState, useCallback} from 'react'
+import {useState, useCallback, useEffect} from 'react'
 
 export const useAuth = () => {
 	
@@ -22,6 +22,15 @@ export const useAuth = () => {
 		localStorage.removeItem(storageName)
 		
 	},[])
+	
+	useEffect(()=>{
+		const data = JSON.parse(localStorage.getItem(storageName))
+		
+		if(data && data.token){
+			login(data.token, data.userId)
+		}
+		
+	}, [login()])
 	
 	return {login, logout, token, userId}
 }
