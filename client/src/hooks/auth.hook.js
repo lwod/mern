d9a2537dd3d8f18b9f207b8cac0829cd061d226e
lwod <lwodcurrentmail@gmail.com>
2021-03-26 21:15:55 +0200

    add auth.hook.js for login at react-part

---
+++ client/src/hooks/auth.hook.js
@@ -0,0 +1,12 @@
+import {useState, useCallback} from 'react'
+
+export const useAuth = () => {
+	
+	const [token, setToken] = useState(null)
+	const [userId, setUserId] = useState(null)
+	
+	const login = useCallback(()=>{},[])
+	const logout = useCallback(()=>{},[])
+	
+	return {login, logout}
+}
