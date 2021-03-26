cb1a4948e9c2196dce5ed303a2a5fd799e2743bf
lwod <lwodcurrentmail@gmail.com>
2021-03-26 21:17:42 +0200

    login at auth.hook.js

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -2,10 +2,20 @@ import {useState, useCallback} from 'react'
 
 export const useAuth = () => {
 	
+	const storageName = 'userData';
+	
 	const [token, setToken] = useState(null)
 	const [userId, setUserId] = useState(null)
 	
-	const login = useCallback(()=>{},[])
+	const login = useCallback((jwtToken, id)=>{
+		setToken(jwtToken)
+		setUserId(id)
+		
+		localStorage.setItem(storageName, JSON.stringify({
+			userId, token
+		}))
+	},[])
+
 	const logout = useCallback(()=>{},[])
 	
 	return {login, logout}
