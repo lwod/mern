a8e4eee1db7b4aff907549eb132b1bd72f9c6f28
lwod <lwodcurrentmail@gmail.com>
2021-03-26 18:26:12 +0200

    add registerHandler to the register-button at AuthPage.js

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -66,8 +66,19 @@ export const AuthPage = () =>{
 					
 					</div>
 					<div className="card-action">
-						<button className={'btn yellow darken-4' } style={{marginRight:10}}>Войти</button>
-						<button className={'btn grey lighten-1 black-text'}>Регистрация</button>
+						<button
+							className={'btn yellow darken-4' }
+							style={{marginRight:10}}
+							disabled={loading}
+						>Войти
+						</button>
+						<button
+							className={'btn grey lighten-1 black-text'}
+							onClick={registerHandler}
+							disabled={loading}
+						>
+							Регистрация
+						</button>
 					</div>
 				</div>
 			</div>
