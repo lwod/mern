65d510a93f0990fffcb906728400d0b2057876f0
lwod <lwodcurrentmail@gmail.com>
2021-03-27 12:02:02 +0200

    create link at the link.routes.js at '/generate' router

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,3 +1,5 @@
+const config = require('config')
+const shortid = require('shortid')
 const Link = require('../models/Link')
 
 const {Router} = require('express')
@@ -7,11 +9,30 @@ const auth = require('../middleware/auth.middleware')
 
 router.post(
 	'/generate',
+	auth,
 	async (req,res)=>{
 		try{
-		
-		
-		
+			const baseUrl = config.get('baseUrl')
+			const {from} = req.body;
+			
+			const code = shortid.generate()
+			
+			const existing = await Link.findOne({from})
+			
+			if(existing){
+				return res.json({link: existing})
+			}
+			
+			const to = baseUrl + '/t/' + code;
+			
+			const link = new Link({
+				code, to, from, owner: req.user.userId
+			})
+			
+			await link.save()
+			
+			res.status(201).json({link})
+			
 		}catch (e) {
 			res.status(500).json({
 				message: 'Что-то пошло не так, попробуйте снова'
@@ -39,6 +60,7 @@ router.get(
 
 router.get(
 	'/:id',
+	auth,
 	async (req,res)=>{
 		try{
 			const link = await Link.findById(req.params.id)
