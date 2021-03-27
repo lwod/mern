dafa3d8427cc4b71740c5ee71ad4a3a782223d7f
lwod <lwodcurrentmail@gmail.com>
2021-03-27 21:25:03 +0200

    basic part of the CreatePage.js

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const CreatePage = () =>{
+
+	const [link, setLink] = useState(null)
+
 	return(
-		<div>
-			<h1>Create Page</h1>
+		<div className={'row'}>
+			<div className={"col s8 offset-s2"} style={{paddingTop: '2rem'}}>
+				<input
+					placeholder={"Вставьте ссылку"}
+					id={"link"}
+					type={"text"}
+					// onChange={}
+				/>
+				<label htmlFor={'link'}></label>
+				
+			</div>
 		</div>
 	)
 }
