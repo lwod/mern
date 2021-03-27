28744b221e0ef87a2daa48d15b090ea9b8bf89b0
lwod <lwodcurrentmail@gmail.com>
2021-03-27 21:35:45 +0200

    add pressHandler method at the CreatePage.js for send http-request to the server with http.hook

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,9 +1,22 @@
 import React, {useState} from "react";
+import {useHttp} from "../hooks/http.hook";
 
 export const CreatePage = () =>{
 
-	const [link, setLink] = useState(null)
+	const [link, setLink] = useState('')
+	const {request} = useHttp()
 
+	const pressHandler = async (event) => {
+		if(event.key === 'Enter'){
+			try{
+				const data = await request('/api/link/generate', 'POST', {from: link})
+				console.log(data);
+			}catch (e) {
+			
+			}
+			
+		}
+	}
 	
 	return(
 		<div className={'row'}>
@@ -14,6 +27,7 @@ export const CreatePage = () =>{
 					type={"text"}
 					value={link}
 					onChange={e => setLink(e.target.value)}
+					onKeyPress={pressHandler}
 				/>
 				<label htmlFor={'link'}>Введите ссылку</label>
 				
