87e99b0d6685d261db06a1ea80f30c2aeef85577
lwod <lwodcurrentmail@gmail.com>
2021-03-27 11:48:20 +0200

    verify middleware by jwt and config

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
 module.exports = (req,res, next) => {
 	if(req.method === 'OPTIONS'){
 		return next();
@@ -10,8 +13,12 @@ module.exports = (req,res, next) => {
 			return res.status(401).json({message: 'Нет авторизации'})
 		}
 		
+		const decoded = jwt.verify(token, config.get('jwtSecret'))
+		req.user = decoded;
+		
+		next()
 		
 	}catch (e) {
-	
+		res.status(401).json({message:'Нет авторизации'})
 	}
 }
