5d89352287775dd51787dab287b6708c3d88ce0f
lwod <lwodcurrentmail@gmail.com>
2021-03-27 11:32:50 +0200

    add try-catch with 500-status to the routers at link.routes.js

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -6,18 +6,41 @@ const router = Router()
 router.post(
 	'/generate',
 	async (req,res)=>{
-	
+		try{
+			
+		
+		}catch (e) {
+			res.status(500).json({
+				message: 'Что-то пошло не так, попробуйте снова'
+			})
+		}
 })
 
 router.get(
 	'/',
 	async (req,res)=>{
+		try{
+		
+		
+		}catch (e) {
+			res.status(500).json({
+				message: 'Что-то пошло не так, попробуйте снова'
+			})
+		}
 	
 })
 
 router(
 	'/:id',
 	async (req,res)=>{
+		try{
+		
+		
+		}catch (e) {
+			res.status(500).json({
+				message: 'Что-то пошло не так, попробуйте снова'
+			})
+		}
 	
 })
 
