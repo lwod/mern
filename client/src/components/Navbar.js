3a79b09ecef979f87756865e04f7ed91934bd7cf
lwod <lwodcurrentmail@gmail.com>
2021-03-27 11:15:00 +0200

    little fix at the Navbar.js move link-text at the tag-area

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,12 +16,12 @@ export const Navbar = () => {
 	
 	return(
 		<nav>
-			<div className="nav-wrapper">
-				<a href="#" className="brand-logo">Сокращение ссылок</a>
+			<div className="nav-wrapper blue darken-1" style={{padding: '0 2rem'}}>
+				<span className="brand-logo">Сокращение ссылок</span>
 				<ul id="nav-mobile" className="right hide-on-med-and-down">
-					<li><NavLink to={'/create'}></NavLink>Создать</li>
-					<li><NavLink to={'/links'}></NavLink>Ссылки</li>
-					<li><a href={'/'} onClick={logoutHandler}></a>Выйти</li>
+					<li><NavLink to={'/create'}>Создать</NavLink></li>
+					<li><NavLink to={'/links'}>Ссылки</NavLink></li>
+					<li><a href={'/'} onClick={logoutHandler}>Выйти</a></li>
 				</ul>
 			</div>
 		</nav>
