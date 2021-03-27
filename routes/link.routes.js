2da7acb7546538d7228ac4fa87d6c3064c6a53d5
lwod <lwodcurrentmail@gmail.com>
2021-03-27 11:36:50 +0200

    add basic logic for get links by owner and get link by linkId

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -20,8 +20,11 @@ router.get(
 	'/',
 	async (req,res)=>{
 		try{
-		
-		
+			
+			const links = await Link.find({owner: null})
+			res.json(links)
+			
+			
 		}catch (e) {
 			res.status(500).json({
 				message: 'Что-то пошло не так, попробуйте снова'
@@ -34,7 +37,8 @@ router(
 	'/:id',
 	async (req,res)=>{
 		try{
-		
+			const link = await Link.findById(req.params.id)
+			res.json(link)
 		
 		}catch (e) {
 			res.status(500).json({
