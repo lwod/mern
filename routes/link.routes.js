22cb074a4619b2ee046bf2bb3dde51c614969c88
lwod <lwodcurrentmail@gmail.com>
2021-03-27 11:29:59 +0200

    add link.routes.js for routing logic with links

---
+++ routes/link.routes.js
@@ -0,0 +1,24 @@
+const Link = require('../models/Link')
+
+const {Router} = require('express')
+const router = Router()
+
+router.post(
+	'/generate',
+	async (req,res)=>{
+	
+})
+
+router.get(
+	'/',
+	async (req,res)=>{
+	
+})
+
+router(
+	'/:id',
+	async (req,res)=>{
+	
+})
+
+module.exports = router()
