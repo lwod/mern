6b1a3d571883ec86fd388ea4a17337580c3c8dc2
lwod <lwodcurrentmail@gmail.com>
2021-03-27 09:47:14 +0200

    wrap App.js to context from AuthContex.js and use isAuthenticated to routing application's pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,24 @@ import 'materialize-css'
 import {useRoutes} from "./routes";
 import {BrowserRouter} from "react-router-dom";
 import {useAuth} from "./hooks/auth.hook";
+import {AuthContext} from "./context/AuthContex";
 
 function App() {
     const {login, logout, token, userId} = useAuth()
-    const routes = useRoutes(false)
+    const isAuthenticated = !!token;
+    
+    const routes = useRoutes(isAuthenticated)
     
   return (
+      <AuthContext.Provider value = {{
+          token, login, logout, userId, isAuthenticated
+      }}>
       <BrowserRouter>
           <div className={"container"}>
               {routes}
           </div>
       </BrowserRouter>
+      </AuthContext.Provider>
   );
 }
 
--- client/src/context/AuthContex.js
+++ client/src/context/AuthContex.js
@@ -1,4 +1,4 @@
-import {useContext} from 'react'
+import {createContext} from 'react'
 
 function noop () {}
 
