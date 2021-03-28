c4483ec79f348a369ee296bd0e6582eecbd70a38
lwod <lwodcurrentmail@gmail.com>
2021-03-28 09:14:38 +0300

    get link at the DetailPage.js by token from AuthContext with request-method from http.hook.js

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, {useState, useCallback, useContext} from "react";
+import {useParams} from 'react-router-dom'
+import {useHttp} from "../hooks/http.hook";
+import {AuthContext} from "../context/AuthContex";
+
 
 export const DetailPage = () =>{
+	
+	const {token} = useContext(AuthContext);
+	
+	const {request, loading} = useHttp()
+	
+	const [link, setLink] = useState(null)
+	const linkId = useParams().id
+	
+	const getLink = useCallback(async ()=>{
+		try {
+			const fetched = await request(`/api/link/${linkId}`, 'GET', null, {
+				Authorization: `Bearer ${token}`
+			})
+			
+			setLink(fetched);
+		}catch (e) {
+		
+		}
+	}, [token, linkId, request])
+	
 	return(
 		<div>
 			<h1>Detail Page</h1>
