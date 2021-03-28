61f107dc8acb9eea8353177f3536ce60db8ae8db
lwod <lwodcurrentmail@gmail.com>
2021-03-28 09:00:52 +0300

    add ready-flag to the auth.hook.js

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -5,6 +5,7 @@ export const useAuth = () => {
 	const storageName = 'userData';
 	
 	const [token, setToken] = useState(null)
+	const [ready, setReady] = useState(false)
 	const [userId, setUserId] = useState(null)
 	
 	const login = useCallback((jwtToken, id)=>{
@@ -29,8 +30,9 @@ export const useAuth = () => {
 		if(data && data.token){
 			login(data.token, data.userId)
 		}
+		setReady(true)
 		
 	}, [login])
 	
-	return {login, logout, token, userId}
+	return {login, logout, token, userId, ready}
 }
